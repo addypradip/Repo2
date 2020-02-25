36a7277bc1030f7d0f56a3edebb4925130d8694a
addypradip <addypradip@gmail.com>
2020-02-25 01:56:26 -0600

    changes made

--- Eclipse_github/src/git_package/Git_class.java
+++ Eclipse_github/src/git_package/Git_class.java
@@ -3,7 +3,21 @@ package git_package;
 public class Git_class {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/* int a[] = new int [5];
+		
+		a[0] = 100;
+		a[1] = 200;
+		a[2] = 300;
+		a[3] = 400;
+		a[4] = 500;
+		*/
+		
+		int a[] = {100,200,300,400,500};
+		
+		System.out.println(a.length);
+		System.out.println(a[4]);
+		
 
 	}
 
